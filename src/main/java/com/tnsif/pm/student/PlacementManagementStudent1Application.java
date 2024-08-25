f0613591710d55093bd6810c519a19a872463b3b
USER <USER@Saravana>
2024-08-25 10:57:35 +0530

    First commit

---
+++ src/main/java/com/tnsif/pm/student/PlacementManagementStudent1Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.pm.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementManagementStudent1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementManagementStudent1Application.class, args);
+	}
+
+}
